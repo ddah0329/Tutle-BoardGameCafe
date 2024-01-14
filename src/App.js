6dcc83da0d65043aca2edc82676af6250d4fbb90
ddah0329 <aldrlsb03@naver.com>
2024-01-14 22:34:01 +0900

    test4

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          akdjkfjkklsdgliguohgkvmnsdhlkfgjuskgjlsgjklksjdklaj마ㅣㄴ어히ㅓ비자허ㅣ;ㅓㅏㅎakakakakakkkakakakakEdit{" "}
-          <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    TEST Edit{" "}
+                    <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
