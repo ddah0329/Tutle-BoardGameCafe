8a738671423f3e20b793421ac3679df71f9c74bc
ddah0329 <aldrlsb03@naver.com>
2024-02-02 14:10:55 +0900

    feat(커서): 커서 이미지 삽입

--- src/App.js
+++ src/App.js
@@ -14,6 +14,27 @@ import dollimpanText from './Images/dollimpan_text.png';
 function App() {
 
 
+// -------마우스 클릭 커서 이벤트-----------------------
+    const [isMouseClick, setIsMouseClick] = useState(false);
+
+    const handleMouseClick = () => {
+        setIsMouseClick(true);
+
+        setTimeout(() => {
+            setIsMouseClick(false);
+        }, 1000);
+    };
+
+    useEffect(() => {
+        window.addEventListener('click', handleMouseClick);
+
+        return () => {
+            window.removeEventListener('click', handleMouseClick);
+        };
+    }, []);
+
+
+// -------스크롤 이벤트-----------------------
     // 스크롤 이벤트 기본 설정
     const [scrollPosition, setScrollPosition] = useState(0);
 
@@ -45,101 +66,103 @@ function App() {
 
     return (
         <div className="App">
+            <div className={`App ${isMouseClick ? 'is-clicked' : ''}`}>
 
-            {/*---헤더---------------------------------------------*/}
-            <header>
-                <h1>App-header</h1>
-            </header>
-            {/*---상단---------------------------------------------*/}
-            <div className="top-part">
+                {/*---헤더---------------------------------------------*/}
+                <header>
+                    <h1>App-header</h1>
+                </header>
+                {/*---상단---------------------------------------------*/}
+                <div className="top-part">
 
-                {/*---스크롤 이벤트 : 안녕 거북이들 환영합니다----------*/}
-                <h1 style={{transform: helloTransform}}>안녕,</h1>
-                <h1 style={{transform: turtlesTransform}}>거북이들</h1>
-                <h1 style={{transform: welcomeTransform}}>환영합니다</h1>
+                    {/*---스크롤 이벤트 : 안녕 거북이들 환영합니다----------*/}
+                    <h1 style={{transform: helloTransform}}>안녕,</h1>
+                    <h1 style={{transform: turtlesTransform}}>거북이들</h1>
+                    <h1 style={{transform: welcomeTransform}}>환영합니다</h1>
 
-                {/*---스크롤 이벤트 : 임시카드 ---------------------*/}
-                <img
-                    src={temporaryCard1}
-                    alt="Temporary Card 1"
-                    className="header-scroll-img"
-                    style={{...card1Style}}
-                />
-
-                <img
-                    src={temporaryCard2}
-                    alt="Temporary Card 2"
-                    className="header-scroll-img"
-                    style={{...card2Style}}
-                />
-
-                <img
-                    src={temporaryCard3}
-                    alt="Temporary Card 3"
-                    className="header-scroll-img"
-                    style={{...card3Style}}
-                />
-            </div>
-
-            {/*---중앙------------------------------------------*/}
-            <div className="mid-part">
-
-                {/*---몇 명이서 왔어--------------------------*/}
-                <div className="how-many-are-you">
-                    {/* 왼쪽 이미지 */}
-                    <img src={aPlaceForTurtle} alt="Turtles Place" className="left-image"/>
-
-                    {/* 오른쪽 텍스트 박스 */}
-                    <div className="mid-slide-box">
-                        <p>혹시 몇 명이서 왔어?</p>
-                        <div>
-                            <p>1명</p>
-                            <p>2명</p>
-                            <p>3명</p>
-                            <p>4명</p>
-                        </div>
-                    </div>
-                </div>
-
-                {/*---어떤거 좋아해? (장르)--------------------*/}
-                <div className="what-do-you-like">
-                    <div className="mid-slide-box">
-                        <p>혹시 어떤거 좋아해?</p>
-                        <div>
-                            <p>추리</p>
-                            <p>파티</p>
-                            <p>마피아</p>
-                            <p>덱 빌딩</p>
-                        </div>
-                    </div>
-                    <img src={turtleBeam} alt="Turtles Beam" className="left-image"/>
-                </div>
-
-                {/*---검색---------------------------------*/}
-                <div className="search-games">
-                    <input
-                        type="text"
-                        placeholder="이 게임이 있을까?"
-                        className="search-input"
+                    {/*---스크롤 이벤트 : 임시카드 ---------------------*/}
+                    <img
+                        src={temporaryCard1}
+                        alt="Temporary Card 1"
+                        className="header-scroll-img"
+                        style={{...card1Style}}
                     />
-                    <span className="search-icon">&#128269;</span>
+
+                    <img
+                        src={temporaryCard2}
+                        alt="Temporary Card 2"
+                        className="header-scroll-img"
+                        style={{...card2Style}}
+                    />
+
+                    <img
+                        src={temporaryCard3}
+                        alt="Temporary Card 3"
+                        className="header-scroll-img"
+                        style={{...card3Style}}
+                    />
                 </div>
 
-                {/*---돌림판---------------------------------*/}
-                <div className="dol-lim-pan">
-                    <img src={dollimpanImg} alt="Dollimpan Image" className="dollimpan-img"/>
-                    <img src={dollimpanText} alt="Dollimpan Text" className="dollimpan-text"/>
+                {/*---중앙------------------------------------------*/}
+                <div className="mid-part">
+
+                    {/*---몇 명이서 왔어--------------------------*/}
+                    <div className="how-many-are-you">
+                        {/* 왼쪽 이미지 */}
+                        <img src={aPlaceForTurtle} alt="Turtles Place" className="left-image"/>
+
+                        {/* 오른쪽 텍스트 박스 */}
+                        <div className="mid-slide-box">
+                            <p>혹시 몇 명이서 왔어?</p>
+                            <div>
+                                <p>1명</p>
+                                <p>2명</p>
+                                <p>3명</p>
+                                <p>4명</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*---어떤거 좋아해? (장르)--------------------*/}
+                    <div className="what-do-you-like">
+                        <div className="mid-slide-box">
+                            <p>혹시 어떤거 좋아해?</p>
+                            <div>
+                                <p>추리</p>
+                                <p>파티</p>
+                                <p>마피아</p>
+                                <p>덱 빌딩</p>
+                            </div>
+                        </div>
+                        <img src={turtleBeam} alt="Turtles Beam" className="left-image"/>
+                    </div>
+
+                    {/*---검색---------------------------------*/}
+                    <div className="search-games">
+                        <input
+                            type="text"
+                            placeholder="이 게임이 있을까?"
+                            className="search-input"
+                        />
+                        <span className="search-icon">&#128269;</span>
+                    </div>
+
+                    {/*---돌림판---------------------------------*/}
+                    <div className="dol-lim-pan">
+                        <img src={dollimpanImg} alt="Dollimpan Image" className="dollimpan-img"/>
+                        <img src={dollimpanText} alt="Dollimpan Text" className="dollimpan-text"/>
+                    </div>
+
                 </div>
 
-            </div>
+                {/*---하단------------------------------------------*/}
+                <div className="bottom-part">
+                    <p>거북이에서는 지금...🐢</p>
+                    <div className="bottom-three-cards">
 
-            {/*---하단------------------------------------------*/}
-            <div className="bottom-part">
-                <p>거북이에서는 지금...🐢</p>
-                <div className="bottom-three-cards">
+                    </div>
 
                 </div>
-
             </div>
         </div>
     );
